e66b667012d4c883fd1a2ad5ca542809216c8fd7
nabielascar <ascarnabiel@gmail.com>
2024-05-04 09:14:32 +0700

    Menambahkan implementasi API mahasiswa

---
+++ modul10_1302220080/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace modul10_1302220080
+{
+    public class Mahasiswa
+    {
+        public String name { get; set; }
+        public String nim { get; set; }
+        public List<String> course { get; set; }
+        public int year { get; set; }
+        public Mahasiswa(String name, String nim, List<String> course,  int year)
+        {
+            this.name = name;
+            this.nim = nim;
+            this.course = course;
+            this.year = year;
+        }
+
+    }
+}
---
+++ modul10_1302220080/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302220080.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<String> course = new List<String> { "PBO", "KPL", "BD", "UX" };
+        private static List<Mahasiswa> arrMahasiswa = new List<Mahasiswa>
+        {
+        new Mahasiswa ("Nabiel Ascar Mochammad", "1302220080",course, 2022),
+        new Mahasiswa("Fauzan Arrizqy Putra", "1302220004", course, 2023),
+        new Mahasiswa("Muhammad Izhar Fahriansyah", "1302220056", course, 2021),
+        new Mahasiswa("Wildan Hadi Fernando", "1302223012", course, 2020)
+        };
+        [HttpGet]
+
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return arrMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            return arrMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Mahasiswa mahasiswa)
+        {
+            arrMahasiswa.Add(mahasiswa);
+           
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            arrMahasiswa.RemoveAt(id);
+        }
+    }
+}
